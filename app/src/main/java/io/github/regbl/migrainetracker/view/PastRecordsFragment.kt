86124c91164f5a5a6a17b07c286db07e2ddf0c76
RegBl <bnash1@madisoncollege.edu>
2022-05-13 12:36:12 -0500

    Added app bar and some search elements, revised list item,

--- app/src/main/java/io/github/regbl/migrainetracker/view/PastRecordsFragment.kt
+++ app/src/main/java/io/github/regbl/migrainetracker/view/PastRecordsFragment.kt
@@ -1,21 +1,18 @@
 package io.github.regbl.migrainetracker.view
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
+import io.github.regbl.migrainetracker.R
 import io.github.regbl.migrainetracker.adapters.PastRecordsAdapter
 import io.github.regbl.migrainetracker.databinding.FragmentPastRecordsBinding
 import io.github.regbl.migrainetracker.viewmodel.PastRecordsViewModel
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class PastRecordsFragment : Fragment() {
-
     private var _binding: FragmentPastRecordsBinding? = null
-    private val binding get() = _binding!!
 
+    private val binding get() = _binding!!
     private val pastRecordsViewModel: PastRecordsViewModel by viewModel()
 
     override fun onCreateView(
@@ -27,11 +24,14 @@ class PastRecordsFragment : Fragment() {
 
         val adapter = PastRecordsAdapter()
         binding.questionnaireList.adapter = adapter
-        binding.questionnaireList.layoutManager = LinearLayoutManager(this.context)
 
         pastRecordsViewModel.questionnaires.observe(viewLifecycleOwner) { adapter.submitList(it) }
 
         return view
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.options_menu, menu)
     }
 
     override fun onDestroy() {
